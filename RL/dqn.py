8ee2f2663112ef84420b128d906cd93d5c6d41fb
Gantulga <limited.tulgaa@gmail.com>
2023-01-25 22:46:06 +0800

    moved gpu init code from DQN.create_model to DQN.__init__

--- RL/dqn.py
+++ RL/dqn.py
@@ -11,35 +11,16 @@ from .utils import ReplayBuffer, DoubleReplayBuffer
 
 class DQN(Agent):
 
-    def __init__(self, action_space: list, lr: float, y: float, e_decay: float = 0.99999) -> None:
+    def __init__(self, action_space: list, lr: float, y: float, e_decay: float = 0.99999, gpu: bool = False) -> None:
         super().__init__(action_space, lr, y, e_decay)
         self.target_model = None
         self.buffer = None
         self.batchs = 0
         self.epochs = 0
-        self.gpu = False
+        self.gpu = gpu
         self.train_freq = 10
         self.update_freq = 10
         self.train_count = 0
-
-    def create_buffer(self, max_size: int, min_size: int = 0, opt: int = 0):
-        """opt - [0] for single buffer, [1] for double buffer"""
-        if min_size == 0:
-            min_size = self.batchs
-        if opt == 0:
-            self.buffer = ReplayBuffer(max_size=max_size, min_size=min_size)
-        elif opt == 1:
-            self.buffer = DoubleReplayBuffer(max_size=max_size, min_size=min_size)
-        else:
-            self.buffer = ReplayBuffer(max_size=max_size, min_size=min_size)
-
-    def create_model(self, sizes: list, batchs: int = 128, epochs: int = 1, gpu: bool = False, train_freq: int = 10, update_freq: int = 10) -> Sequential:
-        """sizes - [state_size, hidden_state, action_size]"""
-        self.batchs = batchs
-        self.epochs = epochs
-        self.gpu = gpu
-        self.train_freq = train_freq
-        self.update_freq = update_freq
         if self.gpu:
             gpus = tf.config.list_physical_devices('GPU')
             if gpus:
@@ -53,6 +34,24 @@ class DQN(Agent):
                     print(e)
         else:
             os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    def create_buffer(self, max_size: int, min_size: int = 0, opt: int = 0):
+        """opt - [0] for single buffer, [1] for double buffer"""
+        if min_size == 0:
+            min_size = self.batchs
+        if opt == 0:
+            self.buffer = ReplayBuffer(max_size=max_size, min_size=min_size)
+        elif opt == 1:
+            self.buffer = DoubleReplayBuffer(max_size=max_size, min_size=min_size)
+        else:
+            self.buffer = ReplayBuffer(max_size=max_size, min_size=min_size)
+
+    def create_model(self, sizes: list, batchs: int = 128, epochs: int = 1, train_freq: int = 10, update_freq: int = 10) -> Sequential:
+        """sizes - [state_size, hidden_state, action_size]"""
+        self.batchs = batchs
+        self.epochs = epochs
+        self.train_freq = train_freq
+        self.update_freq = update_freq
         self.model = Sequential()
         self.target_model = Sequential()
         self.model.add(Input(shape=(sizes[0],)))
