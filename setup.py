1458fb3aa24f2388e32e59b5058be5d276d10bb1
Gantulga G <limited.tulgaa@gmail.com>
2023-09-07 19:02:42 +0800

    setup, requirements change

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="RL",
+    name="PyRL",
     version="0.1-dev",
     author="Gantulga G",
-    author_email="limited.tulgaa@gmail.com>",
+    author_email="limited.tulgaa@gmail.com",
     description="Simple helper module for RL training and testing",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Gantulga9480/RL",
+    url="https://github.com/Gantulga9480/PyRL",
     packages=setuptools.find_packages(),
     license='MIT',
-    install_requires=['numpy', 'matplotlib'],
+    install_requires=['numpy'],
 )
