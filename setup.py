7e417d63f3f7804e86d7273782d8835535bdb570
Gantulga G <limited.tulgaa@gmail.com>
2023-10-07 20:43:50 +0800

    setup.py change

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyRL",
-    version="0.1-dev",
+    version="1.0",
     author="Gantulga G",
     author_email="limited.tulgaa@gmail.com",
-    description="Pytorch implementation for RL training",
+    description="Pytorch implementation of RL algorithms",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Gantulga9480/PyRL",
-    packages=['pyrl'],
+    packages=setuptools.find_packages(),
     license='MIT',
     install_requires=['numpy'],
 )
